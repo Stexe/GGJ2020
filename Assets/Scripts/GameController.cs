0866da521017088ee792add1b54b4cbf503a2302
Stefan Lopuszanski <Stexe@UConn.edu>
2020-02-01 09:22:18 -0500

    SINGLETON POWER! (And Robot Health UI)
    ROOT OF ALL EVIL IS SINGLETON

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField] private HealthBar healthBar;
+
+
+    private void Start()
+    {
+
+
+        FunctionPeriodic.Create(() =>
+        {
+            if (WorldManager.Instance.robotHealth > 0.001f)
+            {
+                //WorldManager.Instance.robotHealth -= .01f;  //Testing decay
+                healthBar.SetSize(WorldManager.Instance.robotHealth);
+
+                if (WorldManager.Instance.robotHealth < 0.3f)
+                {
+                    if ((WorldManager.Instance.robotHealth * 100) % 3 == 0)
+                    {
+                        healthBar.SetColor(Color.white);
+                    }
+                    else
+                    {
+                        healthBar.SetColor(Color.red);
+                    }
+                }
+            }
+        }, .03f);
+    }
+
+}
---
+++ Assets/Scripts/Singleton/WorldManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WorldManager : Singleton<WorldManager>
+{
+
+    // Use with:  WorldManager.Instance.
+
+    public bool gameStarted = false;
+    public bool isDead = false;
+
+    public float globalTime = 0.0f;
+    public float robotHealth = 1.0f;
+
+    public Text globalHUDText;
+
+    //High score variables////////////
+    public float highScore;
+    public float currentScore;
+    public int difficultySettingSpawn = 1;
+    public float timeToBeat;
+    Scene myScene;
+    //////////////////////////////////
+
+    public void Start()
+    {
+        StartCoroutine(LateStart(0.1f));
+    }
+
+    IEnumerator LateStart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        myScene = SceneManager.GetActiveScene();
+        // scoreManager = GameObject.Find("GameManager").GetComponent<ScoreManager>();
+    }
+
+    public void Update()
+    {
+
+        //globalTime = globalTime + Time.deltaTime;
+        //globalHUDText.text = "Score: " + currentScore;
+
+    }
+
+}
