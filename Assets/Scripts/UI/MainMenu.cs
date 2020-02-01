aab95b9c24e6105d7d2c6b023d76707ba40489e3
Stefan Lopuszanski <Stexe@UConn.edu>
2020-01-31 23:19:02 -0500

    Menu Mumbo Jumbo
    Also "Crosshair"

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject titlePanel;
+    public GameObject selectionPanel;
+
+    public void StartGame()
+    {
+
+        titlePanel.SetActive(false);
+        selectionPanel.SetActive(false);
+        SceneManager.LoadScene("Main");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game quit!");
+    }
+	
+}
