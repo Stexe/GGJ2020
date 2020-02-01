5236e076aa15d257789e4bde2d33c7fd4767d39f
Stefan Lopuszanski <Stexe@UConn.edu>
2020-02-01 14:25:15 -0500

    Collection Particle

---
+++ Assets/ParticleKiller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleKiller : MonoBehaviour
+{
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = 0.6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
