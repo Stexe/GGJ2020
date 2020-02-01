1276421df68cef6609ca4d44508d43cecc46c5a7
Patrick Belanger <pbandjoygames@gmail.com>
2020-02-01 10:28:04 -0500

    Added ResourceImage prefabs

--- Assets/Scripts/ResourcesFolder/ResourceObject.cs
+++ Assets/Scripts/ResourcesFolder/ResourceObject.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ResourceObject : MonoBehaviour
 {
+    /// <summary>
+    /// Players can't collect resources for a small amount of time after they're thrown
+    /// </summary>
+    private float ignorePlayerTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +17,14 @@ public class ResourceObject : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if(ignorePlayerTimer <= 0)
+        {
+            
+        }
+    }
+
+    public void MoveToward(Transform other)
     {
         
     }
